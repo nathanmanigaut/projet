238b3998fbf9609b9c91443ecb87966cd72664b2
nathanmanigaut <nathan.manigaut@gmail.com>
2021-02-15 15:04:08 +0100

    modif nav back

--- application/views/back/partials/nav.php
+++ application/views/back/partials/nav.php
@@ -1,16 +1,16 @@
 <nav>
         <ul class="nav nav-pills nav-fill">
           <li class="nav-item border">
-            <a class="nav-link" href="accueil">Accueil</a>
+            <a class="nav-link" href="http://localhost/projet/accueil">Accueil</a>
           </li>
           <li class="nav-item border">
             <a class="nav-link" href="http://localhost/projet/login/signout">Se deconnecter</a>
           </li>
           <li class="nav-item border">
-            <a class="nav-link" href="tournois">Les tournois </a>
+            <a class="nav-link" href="http://localhost/projet/tournois">Les tournois </a>
           </li>
           <li class="nav-item border">
-            <a class="nav-link" href="actu">Actu / Scène professionnelle</a>
+            <a class="nav-link" href="http://localhost/projet/actu">Actu / Scène professionnelle</a>
           </li>
         </ul>
       </nav>
