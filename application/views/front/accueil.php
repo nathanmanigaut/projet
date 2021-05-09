632c2f21c12b5e1ff58685442c5ee3587c5c9099
nathanmanigaut <nathan.manigaut@gmail.com>
2021-05-09 19:42:37 +0200

    page accueil

---
+++ application/views/front/accueil.php
@@ -0,0 +1,18 @@
+<div class="d-flex justify-content-center border" style="height: 150px">
+    <div class="d-flex align-self-center">
+        <p>Esport tournament le site pour créer et s'inscrire à des tournois amateurs sur différents jeux vidéos</p>
+    </div>
+</div>
+<div class="d-flex justify-content-center border" style="height: 200px">
+    <div class=" align-self-center">
+            <h6> <a href="<?base_url('login')?>">Créer un compte</a> c'est pouvoir:</h6>
+         
+        <ul>
+                <li>Créer votre équipe </li>
+                <li>Inscrire votre équipe à un tournoi </li>
+                <li>Créer votre propre tournoi </li>
+            </ul> 
+             
+    </div>
+            
+</div>
